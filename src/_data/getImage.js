3d44d04ab00cd229097fe15621895f4620d2695b
seriAlizations <124045671+seriAlizations@users.noreply.github.com>
2023-05-26 13:02:36 +0000

    test: api fetch works!

--- src/_data/getImage.js
+++ src/_data/getImage.js
@@ -4,9 +4,9 @@ async function getImage() {
   const api = "https://api.nasa.gov/planetary/apod?api_key=P2cQAUnjmawmIKboFijT58RDdQYLZ5wEGZxdLdDY";
   const response = await EleventyFetch(api, {
     duration: "1d",
-    type: "json"
+    type: "buffer"
   });
-  const hdImage = response.hdurl; 
+  const hdImage = response; 
   return hdImage; 
 }
 module.exports = getImage;
